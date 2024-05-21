0350fffa960fe4ea3a197b8f1ba8db33452a7652
unknown <zandertabelona@gmail.com>
2024-05-21 09:00:07 +0800

    user view

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -3,16 +3,17 @@
         <h2 class="text-2xl font-bold text-gray-700">Welcome, User!</h2>
 
         <div class="flex mt-8 space-x-6">
-            <div class="card w-64 h-36 bg-gray-200 rounded-md shadow-sm p-4">
+            <div class="card w-64 h-36 bg-white rounded-md shadow-sm p-4">
                 <span class="font-bold text-5xl text-blue-700 ml-4">2,400</span>
+                <p class="ml-4 mt-4 font-bold text-gray-700">Total Partnerships Forged</p>
             </div>
-            <div class="card w-64 h-36 bg-gray-200 rounded-md shadow-sm p-4">
+            <div class="card w-64 h-36 bg-white rounded-md shadow-sm p-4">
                 <span class="font-bold text-5xl text-blue-700 ml-4">987</span>
             </div>
-            <div class="card w-64 h-36 bg-gray-200 rounded-md shadow-sm p-4">
-                <span class="font-bold text-5xl text-blue-700 ml-4">567</span>
+            <div class="card w-64 h-36 bg-white rounded-md shadow-sm p-4">
+                <span class="font-bold text-5xl text-blue-700 ml-4">75%</span>
             </div>
-            <div class="card w-64 h-36 bg-gray-200 rounded-md shadow-sm p-4">
+            <div class="card w-64 h-36 bg-white rounded-md shadow-sm p-4">
                 <span class="font-bold text-5xl text-blue-700 ml-4">900</span>
             </div>
         </div>
